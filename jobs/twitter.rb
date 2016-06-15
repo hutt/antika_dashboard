111390db2c0b7b6579ddf4c407c4f146d36ff4e0
Dashboard <dashboard@jh0.eu>
2016-06-15 11:19:55 -0400

    edited twitter job

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,28 +1,21 @@
 require 'twitter'
 
-
-#### Get your twitter keys & secrets:
-#### https://dev.twitter.com/docs/auth/tokens-devtwittercom
-twitter = Twitter::REST::Client.new do |config|
+Twitter.configure do |config|
+  #Berechtigungen: read-only.
   config.consumer_key = 'XvzGSGPT6RNz3nNjLActIDRaV'
   config.consumer_secret = 'DI6C3HHbPgPUGMJO9i4dgKXBQmuuUlr87cOuKsn9Ivg4MMVd8R'
-  config.access_token = '91400607-qt4ZciqjUzPzIXcOe3EHZ9nT2Mh37FWykpKCJSfKF'
-  config.access_token_secret = '91400607-qt4ZciqjUzPzIXcOe3EHZ9nT2Mh37FWykpKCJSfKF'
+  config.oauth_token = '91400607-qt4ZciqjUzPzIXcOe3EHZ9nT2Mh37FWykpKCJSfKF'
+  config.oauth_token_secret = '3tbyiUeMEEU8cgRLA4xi22W3Oj8ui6L25eVgyb9m0WMXu'
 end
 
-search_term = URI::encode('from:socialmeera')
+search_term = URI::encode('#bge')
 
-SCHEDULER.every '2s', :first_in => 0 do |job|
-  begin
-    tweets = twitter.search("#{search_term}")
-
-    if tweets
-      tweets = tweets.map do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
-      end
-      send_event('twitter_mentions', comments: tweets)
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  tweets = Twitter.search("#{search_term}").results
+  if tweets
+    tweets.map! do |tweet|
+      { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
     end
-  rescue Twitter::Error
-    puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
+    send_event('twitter_mentions', comments: tweets)
   end
 end
